82d5d90c5b2874c27b45c5b752e4923e24572820
yeongjin <dudwls901@naver.com>
2022-05-22 08:01:00 +0900

    fcm 완료

--- app/src/main/java/com/hackerton/shimton/view/HomeActivity.kt
+++ app/src/main/java/com/hackerton/shimton/view/HomeActivity.kt
@@ -2,9 +2,13 @@ package com.hackerton.shimton.view
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,8 +17,11 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.hackerton.shimton.R
 import com.hackerton.shimton.data.remote.dto.Room
 import com.hackerton.shimton.databinding.ActivityHomeBinding
@@ -25,6 +32,10 @@ import com.hackerton.shimton.view.adapter.RoomAdapter
 import com.hackerton.shimton.view.listener.setOnSingleClickListener
 import com.hackerton.shimton.viewmodel.RoomViewModel
 import com.ssafy.smartstore.event.EventObserver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "HomeActivity_sss"
 
@@ -39,11 +50,46 @@ class HomeActivity : AppCompatActivity() {
     private var roomId: Long? = null
     private var roomName: String? = null
 
+    private var notiData: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Log.d(TAG, "onCreate: ")
+
+        // FCM 토큰 수신
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG + "FCM", "FCM 토큰 얻기에 실패하였습니다.", task.exception)
+                return@OnCompleteListener
+            }
+
+            // token 전송
+//            CoroutineScope(Dispatchers.Main).launch {
+//                TokenRepository.INSTANCE.uploadToken(userId, task.result ?: "task.result is null")
+//            }
+            Log.d(TAG + "FCM", "token: ${task.result ?: "task.result is null"}")
+
+        })
+
+        setFCMChannel()
+
+        //background push 데이터 받기
+        notiData = intent?.getStringExtra("notiData")
+        Log.d(TAG, "notidata onCreate: $notiData")
+        notiData?.let {
+//            launch {
+//                withContext(Dispatchers.Main) {
+//                    notiRepo.insert(Noti(userId, it))
+//                }
+//                val notiList = notiRepo.select(userId)
+//                Log.d(TAG, "onCreate: ${notiList}")
+//                homeViewModel.updateNotiList(notiList)
+//
+//            }
+        }
+
         initViews()
         observeDatas()
     }
@@ -115,6 +161,33 @@ class HomeActivity : AppCompatActivity() {
             roomViewModel.searchRoomList(data)
         }
 
+
+    }
+
+
+    // FCM 노티 채널 설정
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setFCMChannel() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            if (!it.isSuccessful) {
+                Log.d(TAG, "실패: ${it.exception}")
+            } else {
+                Log.d(TAG, "성공: ${it.result}")
+            }
+        }
+        createNotiChannel("ssafy_id", "ssafy")
+    }
+
+    //오레오 이상에서만 동작하는 함수
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotiChannel(channelId: String, channelName: String) {
+
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, channelName, importance)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
 
     }
 
